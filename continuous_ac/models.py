52ae9ff2c95af979bcabf3f0f097f5ac3f49d471
koulanurag <koulanurag@gmail.com>
2023-01-13 14:08:01 -0500

    feat: wandb init and cuda setup

--- continuous_ac/models.py
+++ continuous_ac/models.py
@@ -9,20 +9,22 @@ import numpy as np
 
 from vit import ViT
 
-#from batchrenorm import BatchRenorm1d
+# from batchrenorm import BatchRenorm1d
 
 ce = nn.CrossEntropyLoss()
 
+
 class GaussianFourierProjectionTime(nn.Module):
-  """Gaussian Fourier embeddings for noise levels."""
+    """Gaussian Fourier embeddings for noise levels."""
 
-  def __init__(self, embedding_size=256, scale=1.0):
-    super().__init__()
-    self.W = nn.Parameter(torch.randn(embedding_size) * scale, requires_grad=False)
+    def __init__(self, embedding_size=256, scale=1.0):
+        super().__init__()
+        self.W = nn.Parameter(torch.randn(embedding_size) * scale, requires_grad=False)
 
-  def forward(self, x):
-    x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
-    return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+    def forward(self, x):
+        x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
+        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
+
 
 class ResMLP(nn.Module):
     def __init__(self, n_hidden):
@@ -33,11 +35,12 @@ class ResMLP(nn.Module):
         self.mlp4 = nn.Linear(n_hidden, n_hidden)
 
     def forward(self, U):
-        z = self.layer_norm(U)                  # z: [B, n_tokens, n_channel]
-        z = self.gelu(self.mlp3(z))             # z: [B, n_tokens, n_hidden]
-        z = self.mlp4(z)                        # z: [B, n_tokens, n_channel]
-        Y = U + z                               # Y: [B, n_tokens, n_channel]
+        z = self.layer_norm(U)  # z: [B, n_tokens, n_channel]
+        z = self.gelu(self.mlp3(z))  # z: [B, n_tokens, n_hidden]
+        z = self.mlp4(z)  # z: [B, n_tokens, n_channel]
+        Y = U + z  # Y: [B, n_tokens, n_channel]
         return Y
+
 
 class AC(nn.Module):
     def __init__(self, din, nk, nact):
@@ -45,18 +48,18 @@ class AC(nn.Module):
 
         self.kemb = nn.Embedding(nk, din)
 
-        self.m = nn.Sequential(nn.Linear(din*3, 256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256,256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, nact))
-        #self.m = nn.Sequential(nn.Linear(din*3, 512), ResMLP(512), ResMLP(512), nn.Linear(512, nact))
+        self.m = nn.Sequential(nn.Linear(din * 3, 256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, 256),
+                               nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, nact))
+        # self.m = nn.Sequential(nn.Linear(din*3, 512), ResMLP(512), ResMLP(512), nn.Linear(512, nact))
 
-        #self.yl = nn.Linear(2,din)
-        #self.tl = GaussianFourierProjectionTime(din//2)
+        # self.yl = nn.Linear(2,din)
+        # self.tl = GaussianFourierProjectionTime(din//2)
 
-    def forward(self, st, stk, k, a): 
-
-        #noise_t = torch.rand_like(a[:,0])
-        #noised_a,_,_ = noised(a, noise_t)
-        #yemb = self.yl(noised_a)
-        #temb = self.tl(noise_t)
+    def forward(self, st, stk, k, a):
+        # noise_t = torch.rand_like(a[:,0])
+        # noised_a,_,_ = noised(a, noise_t)
+        # yemb = self.yl(noised_a)
+        # temb = self.tl(noise_t)
 
         ke = self.kemb(k)
 
@@ -64,59 +67,62 @@ class AC(nn.Module):
 
         h = self.m(h)
 
-
-        loss = ((h - a)**2).mean(dim=-1).sum()
+        loss = ((h - a) ** 2).mean(dim=-1).sum()
 
         return loss
+
 
 class LatentForward(nn.Module):
     def __init__(self, dim, nact):
         super().__init__()
-        self.net = nn.Sequential(nn.Linear(dim+nact, 256), nn.LeakyReLU(), nn.Linear(256, 256), nn.LeakyReLU(), nn.Linear(256, dim))
+        self.net = nn.Sequential(nn.Linear(dim + nact, 256), nn.LeakyReLU(), nn.Linear(256, 256), nn.LeakyReLU(),
+                                 nn.Linear(256, dim))
 
     def forward(self, z, a):
-        return self.net(torch.cat([z.detach(),a],dim=1))
+        return self.net(torch.cat([z.detach(), a], dim=1))
 
     def loss(self, z, zn, a):
         zn = zn.detach()
 
-        zpred = self.net(torch.cat([z.detach(), a.detach()],dim=1))
+        zpred = self.net(torch.cat([z.detach(), a.detach()], dim=1))
 
-        loss = ((zn - zpred)**2).mean(dim=-1).sum()
+        loss = ((zn - zpred) ** 2).mean(dim=-1).sum()
 
         return loss, zpred
+
 
 class Encoder(nn.Module):
     def __init__(self, din, dout):
         super().__init__()
 
-        self.mixer = mixer.MLP_Mixer(n_layers=2, n_channel=32, n_hidden=32, n_output=256, image_size=100, patch_size=10, n_image_channel=3)
+        self.mixer = mixer.MLP_Mixer(n_layers=2, n_channel=32, n_hidden=32, n_output=256, image_size=100, patch_size=10,
+                                     n_image_channel=3)
 
-        #self.vit = ViT(image_size=100, patch_size=10, num_classes_1=256, dim=256, depth=6, heads=4, mlp_dim=512, channels=1)
+        # self.vit = ViT(image_size=100, patch_size=10, num_classes_1=256, dim=256, depth=6, heads=4, mlp_dim=512, channels=1)
 
-        #self.m = nn.Sequential(ResMLP(256), ResMLP(256), nn.Linear(256, dout))
-        self.m = nn.Sequential(nn.Linear(256, 256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256,256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, dout))
+        # self.m = nn.Sequential(ResMLP(256), ResMLP(256), nn.Linear(256, dout))
+        self.m = nn.Sequential(nn.Linear(256, 256), nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, 256),
+                               nn.BatchNorm1d(256), nn.LeakyReLU(), nn.Linear(256, dout))
 
     def forward(self, x):
-
         x = x.reshape((x.shape[0], 1, 100, 100))
 
-        #x = TF.gaussian_blur(x,7)*16
-        #x = TF.gaussian_blur(x,19)*32
+        # x = TF.gaussian_blur(x,7)*16
+        # x = TF.gaussian_blur(x,19)*32
 
-        p1 = torch.arange(0,1,0.01).reshape((1, 1, 100, 1)).repeat((x.shape[0], 1, 1, 100)).cuda()
-        p2 = torch.arange(0,1,0.01).reshape((1, 1, 1, 100)).repeat((x.shape[0], 1, 100, 1)).cuda()
-        x = torch.cat([x,p1,p2],dim=1)
-        
+        p1 = torch.arange(0, 1, 0.01).reshape((1, 1, 100, 1)).repeat((x.shape[0], 1, 1, 100)).to(x.device)
+        p2 = torch.arange(0, 1, 0.01).reshape((1, 1, 1, 100)).repeat((x.shape[0], 1, 100, 1)).to(x.device)
+        x = torch.cat([x, p1, p2], dim=1)
+
         h = self.mixer(x)
 
-        #p1 = (x.round()==1).nonzero(as_tuple=True)[2].unsqueeze(1).float().round() / 100.0
-        #p2 = (x.round()==1).nonzero(as_tuple=True)[3].unsqueeze(1).float().round() / 100.0
+        # p1 = (x.round()==1).nonzero(as_tuple=True)[2].unsqueeze(1).float().round() / 100.0
+        # p2 = (x.round()==1).nonzero(as_tuple=True)[3].unsqueeze(1).float().round() / 100.0
 
-        #print(p1[0])
-        #print(p2[0])
+        # print(p1[0])
+        # print(p2[0])
 
-        #h = torch.cat([p1,p2], dim=1)
+        # h = torch.cat([p1,p2], dim=1)
 
         return self.m(h)
 
@@ -125,33 +131,27 @@ class Probe(nn.Module):
     def __init__(self, din, dout):
         super().__init__()
 
-        #self.enc = nn.Sequential(nn.Linear(din, 512), nn.BatchNorm1d(512), nn.LeakyReLU(), nn.Linear(512,512), nn.BatchNorm1d(512), nn.LeakyReLU(), nn.Linear(512, dout))
-        self.enc = nn.Sequential(nn.Linear(din, 512), nn.LeakyReLU(), nn.Linear(512,512), nn.LeakyReLU(), nn.Linear(512, dout))
+        # self.enc = nn.Sequential(nn.Linear(din, 512), nn.BatchNorm1d(512), nn.LeakyReLU(), nn.Linear(512,512), nn.BatchNorm1d(512), nn.LeakyReLU(), nn.Linear(512, dout))
+        self.enc = nn.Sequential(nn.Linear(din, 512), nn.LeakyReLU(), nn.Linear(512, 512), nn.LeakyReLU(),
+                                 nn.Linear(512, dout))
 
     def forward(self, s):
         return self.enc(s)
 
     def loss(self, s, gt):
+        # print('input', s[:10])
+        # print('target', gt[:10])
+        # print('input-target diff', (torch.abs(s - gt)).sum())
 
-        #print('input', s[:10])
-        #print('target', gt[:10])
-        #print('input-target diff', (torch.abs(s - gt)).sum())
-
-        #print('in probe')
-        #print(s[0])
+        # print('in probe')
+        # print(s[0])
 
         sd = s.detach()
 
         sd = self.enc(sd)
 
-        loss = ((sd - gt)**2).sum(dim=-1).mean()
+        loss = ((sd - gt) ** 2).sum(dim=-1).mean()
 
         abs_loss = torch.abs(sd - gt).mean()
 
         return loss, abs_loss
-
-
-
-
-
-
--- continuous_ac/room_env.py
+++ continuous_ac/room_env.py
@@ -16,20 +16,22 @@ import random
 import torchvision.transforms.functional as F
 import torch
 
+
 def div_cast(x, m=100):
     for i in range(len(x)):
         e = x[i]
-        e = round(e*m)
-        e = e/m
+        e = round(e * m)
+        e = e / m
         x[i] = e
     return x
+
 
 class RoomEnv:
 
     def __init__(self):
-        self.agent_pos = [0,0]
+        self.agent_pos = [0, 0]
 
-    def random_action(self): 
+    def random_action(self):
 
         delta = [0, 0]
         delta[0] = random.uniform(-0.1, 0.1)
@@ -60,43 +62,41 @@ class RoomEnv:
 
     def blur_obs(self, x):
         x = torch.Tensor(x).unsqueeze(0).unsqueeze(0)
-        x = F.gaussian_blur(x,7)*16
+        x = F.gaussian_blur(x, 7) * 16
         x = x.squeeze(0).squeeze(0).numpy()
         return x
 
     def synth_obs(self, ap):
         x = np.zeros(shape=(100, 100))
 
-        x[int(round(ap[0]*100)), int(round(ap[1]*100))] += 1
+        x[int(round(ap[0] * 100)), int(round(ap[1] * 100))] += 1
 
-        #x = self.blur_obs(x)
+        # x = self.blur_obs(x)
 
         return x.flatten()
 
     def get_obs(self):
         x = np.zeros(shape=(100, 100))
 
-        x[int(round(self.agent_pos[0]*100)), int(round(self.agent_pos[1]*100))] += 1
+        x[int(round(self.agent_pos[0] * 100)), int(round(self.agent_pos[1] * 100))] += 1
 
-        #x = self.blur_obs(x)
+        # x = self.blur_obs(x)
 
-        #x = np.concatenate([x, self.exo.reshape((self.m,self.m))], axis=1)
+        # x = np.concatenate([x, self.exo.reshape((self.m,self.m))], axis=1)
 
         exo = [0.0, 0.0]
 
         return x.flatten(), self.agent_pos, exo
 
+
 if __name__ == "__main__":
 
     env = RoomEnv()
 
-    for i in range(0,5000):
+    for i in range(0, 5000):
         a = env.random_action()
         print('s', env.agent_pos)
         print('a', a)
-        x,_,_ = env.get_obs()
+        x, _, _ = env.get_obs()
         print('x-argmax', x.argmax())
         env.step(a)
-
-
-
